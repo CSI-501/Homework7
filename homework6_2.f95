7e470e0a99501e1246c634347079047cdbea0f32
Nick Maynard <33676065+nickmayn@users.noreply.github.com>
2023-03-10 15:03:28 +0000

    backup work

--- homework6_2.f95
+++ homework6_2.f95
@@ -23,9 +23,12 @@ program homework6_2
         read(42,*) x, y
         Sxx = Sxx + ((float(i) - 1)/float(i)) * (x - xAve)**2
         Sxy = Sxy + ((float(i) - 1)/float(i)) * (x - xAve) * (y - yAve)
-        Sxx2 = ((x - xAve)*(x**2 - XAve2)) / n
-        Sx2x2 = (x**2 - XAve2)**2 / n
-        Sx2y = ((y - yAve)*(x**2 - XAve2)) / n
+        ! Sxx2 = ((x - xAve)*(x**2 - XAve2)) / n
+        Sxx2 = x**3 - (n*xAve*xAve2)
+        ! Sx2x2 = (x**2 - XAve2)**2 / n
+        Sx2x2 = x**4 - ((n*xAve2)*xAve2)
+        ! Sx2y = ((y - yAve)*(x**2 - XAve2)) / n
+        Sx2y = (x**2 * y) - (n*xAve2*yAve)
         xAve = xAve + (x - xAve) / float(i)
         xAve2 = xAve2 + (x**2 - xAve2) / float(i)
         yAve = yAve + (y - yAve) / float(i)
@@ -35,7 +38,7 @@ program homework6_2
     enddo
     
     ! Write our output
-    write(13,*) a, b, cff
+    write(13,*) a, b, c
 
     ! Close our Folders
     close(42)
